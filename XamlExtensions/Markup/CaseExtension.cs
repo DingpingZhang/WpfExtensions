92173d9ce7029a6b6238a64448bb936ff964ef9e
DingpingZhang <DingpingZhang@outlook.com>
2019-02-14 11:25:16 +0800

    Add some code for the unfinished SwitchExtension.

---
+++ XamlExtensions/Markup/CaseExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace XamlExtensions.Markup
+{
+    [MarkupExtensionReturnType(typeof(CaseExtension))]
+    public class CaseExtension : MarkupExtension
+    {
+        public const int InvalidIndex = -1;
+
+        [ConstructorArgument(nameof(Option))]
+        public object Option { get; set; }
+
+        [ConstructorArgument(nameof(Value))]
+        public object Value { get; set; }
+
+        internal int Index { get; set; } = InvalidIndex;
+
+        public override object ProvideValue(IServiceProvider serviceProvider) => this;
+    }
+}
