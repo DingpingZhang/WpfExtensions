d120812e03639bb9a711cb9ca12903904d82cffa
Dingping Zhang <DingpingZhang@outlook.com>
2021-06-20 14:47:33 +0800

    Update demo for data-binding.

---
+++ WpfApp.Net462/Views/BindingDemo.xaml.cs
@@ -0,0 +1,16 @@
+﻿using WpfApp.Net462.ViewModels;
+
+namespace WpfApp.Net462.Views
+{
+    /// <summary>
+    /// Interaction logic for BindingDemo.xaml
+    /// </summary>
+    public partial class BindingDemo
+    {
+        public BindingDemo()
+        {
+            DataContext = new BindingDemoViewModel();
+            InitializeComponent();
+        }
+    }
+}
