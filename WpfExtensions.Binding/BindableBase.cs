1843f569b5841e38957031c6bb880c086b1f7bdb
Dingping Zhang <dingpingzhang@outlook.com>
2023-07-21 18:06:28 +0800

    chore(docs): add code comments to all public members.

--- WpfExtensions.Binding/BindableBase.cs
+++ WpfExtensions.Binding/BindableBase.cs
@@ -6,6 +6,9 @@ using System.Runtime.CompilerServices;
 
 namespace WpfExtensions.Binding;
 
+/// <summary>
+/// The common features of <see cref="INotifyPropertyChanged"/> are implemented.
+/// </summary>
 public abstract class BindableBase : INotifyPropertyChanged
 {
     private readonly IDictionary<string, IStrongBox> _cache = new Dictionary<string, IStrongBox>();
@@ -13,6 +16,13 @@ public abstract class BindableBase : INotifyPropertyChanged
     /// <inheritdoc/>
     public event PropertyChangedEventHandler? PropertyChanged;
 
+    /// <summary>
+    /// Computed property: Watch for changes to an expression and automatically notify UI updates when it changes.
+    /// </summary>
+    /// <typeparam name="T">The type of the expression.</typeparam>
+    /// <param name="expression">The computed expression.</param>
+    /// <param name="propertyName">The changed property name.</param>
+    /// <returns>The computed result.</returns>
     protected T Computed<T>(Expression<Func<T>> expression, [CallerMemberName] string propertyName = null!)
     {
         if (!_cache.ContainsKey(propertyName))
@@ -31,6 +41,14 @@ public abstract class BindableBase : INotifyPropertyChanged
         return ((StrongBox<T>)_cache[propertyName]).Value;
     }
 
+    /// <summary>
+    /// An implementation of the property setter that automatically triggers the <see cref="INotifyPropertyChanged"/> event.
+    /// </summary>
+    /// <typeparam name="T">The type of this property.</typeparam>
+    /// <param name="storage">The back field of this property.</param>
+    /// <param name="value">The new value.</param>
+    /// <param name="propertyName">The name of this property.</param>
+    /// <returns>Returns a bool value indicating whether a change has occurred.</returns>
     protected virtual bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string? propertyName = null)
     {
         if (EqualityComparer<T>.Default.Equals(storage, value)) return false;
@@ -41,6 +59,15 @@ public abstract class BindableBase : INotifyPropertyChanged
         return true;
     }
 
+    /// <summary>
+    /// An implementation of the property setter that automatically triggers the <see cref="INotifyPropertyChanged"/> event.
+    /// </summary>
+    /// <typeparam name="T">The type of this property.</typeparam>
+    /// <param name="storage">The back field of this property.</param>
+    /// <param name="value">The new value.</param>
+    /// <param name="onChanged">A callback function that will be called when the property changes.</param>
+    /// <param name="propertyName">The name of this property.</param>
+    /// <returns>Returns a bool value indicating whether a change has occurred.</returns>
     protected virtual bool SetProperty<T>(ref T storage, T value, Action onChanged, [CallerMemberName] string? propertyName = null)
     {
         if (EqualityComparer<T>.Default.Equals(storage, value)) return false;
@@ -52,11 +79,19 @@ public abstract class BindableBase : INotifyPropertyChanged
         return true;
     }
 
+    /// <summary>
+    /// Raises the <see cref="INotifyPropertyChanged.PropertyChanged"/> event.
+    /// </summary>
+    /// <param name="propertyName">The changed property name.</param>
     protected void RaisePropertyChanged([CallerMemberName] string? propertyName = null)
     {
         OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
     }
 
+    /// <summary>
+    /// Raises the <see cref="INotifyPropertyChanged.PropertyChanged"/> event.
+    /// </summary>
+    /// <param name="e">The arguments of this event.</param>
     protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
     {
         PropertyChanged?.Invoke(this, e);
--- WpfExtensions.Binding/Extensions.cs
+++ WpfExtensions.Binding/Extensions.cs
@@ -6,11 +6,40 @@ using System.Linq.Expressions;
 
 namespace WpfExtensions.Binding;
 
+/// <summary>
+/// A callback delegate used to clean up the results of the last call.
+/// </summary>
+/// <param name="cleanup">The logic of clean up.</param>
 public delegate void OnCleanup(Action cleanup);
+
+/// <summary>
+/// A callback delegate it contains an <see cref="OnCleanup"/> parameter
+/// that can be used to clean up the results of the last call
+/// and is generally used in asynchronous callbacks.
+/// </summary>
+/// <typeparam name="T">The type of the value.</typeparam>
+/// <param name="value">The new value after the change.</param>
+/// <param name="oldValue">The old value before the change.</param>
+/// <param name="onCleanup">It used to clean up the results of the last call.</param>
 public delegate void WatchCallback<in T>(T value, T oldValue, OnCleanup onCleanup);
 
+/// <summary>
+/// Extension methods for <see cref="IReactivity"/> interface.
+/// </summary>
 public static class Extensions
 {
+    /// <summary>
+    /// Watches an expression and invokes a callback function when the expression change.
+    /// </summary>
+    /// <typeparam name="T">The type of the expression.</typeparam>
+    /// <param name="self">The <see cref="IReactivity"/> instance.</param>
+    /// <param name="expression">The watched expression.</param>
+    /// <param name="callback">
+    /// A callback function it contains an <see cref="OnCleanup"/> parameter
+    /// that can be used to clean up the results of the last call
+    /// and is generally used in asynchronous callbacks.
+    /// </param>
+    /// <returns>Returns a token for unwatching.</returns>
     public static IDisposable Watch<T>(this IReactivity self, Expression<Func<T>> expression, WatchCallback<T> callback)
     {
         Action? cleanup = null;
@@ -22,6 +51,17 @@ public static class Extensions
         });
     }
 
+    /// <summary>
+    /// Watches an expression and invokes a callback function when the expression change.
+    /// </summary>
+    /// <typeparam name="T">The type of the expression.</typeparam>
+    /// <param name="self">The <see cref="IReactivity"/> instance.</param>
+    /// <param name="expression">The watched expression.</param>
+    /// <param name="callback">
+    /// A callback function it contains two parameters,
+    /// the new and old values before and after the change.
+    /// </param>
+    /// <returns>Returns a token for unwatching.</returns>
     public static IDisposable Watch<T>(this IReactivity self, Expression<Func<T>> expression, Action<T, T> callback)
     {
         Func<T> getter = expression.Compile();
@@ -34,17 +74,40 @@ public static class Extensions
         });
     }
 
+    /// <summary>
+    /// Watches an expression and invokes a callback function when the expression change.
+    /// </summary>
+    /// <typeparam name="T">The type of the expression.</typeparam>
+    /// <param name="self">The <see cref="IReactivity"/> instance.</param>
+    /// <param name="expression">The watched expression.</param>
+    /// <param name="callback">
+    /// A callback function it contains one parameters, the new value after the change.
+    /// </param>
+    /// <returns>Returns a token for unwatching.</returns>
     public static IDisposable Watch<T>(this IReactivity self, Expression<Func<T>> expression, Action<T> callback)
     {
         Func<T> getter = expression.Compile();
         return self.Watch(expression, () => callback(getter()));
     }
 
+    /// <summary>
+    /// Watches a <see cref="INotifyPropertyChanged"/> or <see cref="INotifyCollectionChanged"/> object deeply
+    /// and invokes a callback function when properties in this object changed.
+    /// </summary>
+    /// <param name="self">The <see cref="IReactivity"/> instance.</param>
+    /// <param name="target">The watched object, which must be <see cref="INotifyPropertyChanged"/> or <see cref="INotifyCollectionChanged"/> type.</param>
+    /// <param name="callback">A callback function without parameters.</param>
+    /// <returns>Returns a token for unwatching.</returns>
     public static IDisposable WatchDeep(this IReactivity self, object target, Action callback)
     {
         return self.WatchDeep(target, _ => callback());
     }
 
+    /// <summary>
+    /// Run a <see cref="Action"/> and collect the unwatching tokens from it into the <see cref="Scope"/>.
+    /// </summary>
+    /// <param name="scope">The watching scope.</param>
+    /// <param name="action">The action contains a series of subscription methods.</param>
     public static void Run(this Scope scope, Action action)
     {
         using (scope.Begin())
